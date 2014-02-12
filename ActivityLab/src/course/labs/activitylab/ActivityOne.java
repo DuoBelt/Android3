1155c3f477b8e2ab6bdb04df316fa3ae683b71ec
DuoBelt <mariuszwziatek@gmail.com>
2014-02-12 13:40:29 +0100

    //Git Test 2

--- ActivityLab/src/course/labs/activitylab/ActivityOne.java
+++ ActivityLab/src/course/labs/activitylab/ActivityOne.java
@@ -9,7 +9,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-//Git Test
+//Git Test 2
 public class ActivityOne extends Activity {
 	
 	private static final String RESTART_KEY = "restart";
